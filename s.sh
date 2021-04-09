43e7177e2773029de6376d11a7d317abb6a21b70
abhishrijoshi <abhishrijoshiofficial@gmail.com>
2021-04-09 19:04:42 +0000

    thanks html for being so forgivig and thanks morden browser

--- s.sh
+++ s.sh
@@ -2,6 +2,7 @@ echo the title ?
 read title
 vim ./md/$title.md 
 pandoc ./md/$title.md -t html -o ./html/$title.html 
+echo " <link rel="stylesheet" href="./style.css"> "  >> ./html/$title.html
 sed -i -e "/<!--add-->/a $(cd html && echo \<a href=\"\./html/$(ls -t |head -1)\")"\>$(cd html && ls -t | head -1 )\<\/\a\> index.html
 echo Done 
 
